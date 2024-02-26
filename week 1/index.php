4baf3c08f90dc7b9d452a68b092b62b9dbd6aa49
anan442 <125949078+anan442@users.noreply.github.com>
2024-02-26 19:50:42 +0800

    Update index.php

--- week 1/index.php
+++ week 1/index.php
@@ -1,1 +1,16 @@
+<!Doctype HTML>
 
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <title>Week1 Homework</title>
+	    <link rel="stylesheet" href="">
+	</head>
+
+	<body>
+		<?php
+			echo "<p1>Welcome to PHP!</p1>";
+		?>
+	</body>
+</html>
